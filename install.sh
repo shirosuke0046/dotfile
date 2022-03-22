58f57ee118c1ffe6af06f75c443ed59840979383
shirosuke0046 <shirosuke.0046@gmail.com>
2022-03-23 00:21:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ mkdir -p $PREFIX || exit
 
 git clone https://github.com/shirosuke0046/dotfiles.git $PREFIX/dotfiles
 
-for file in .vimrc; do
-	ln -sf $PREFIX/dotfiles/$file $HOME/$file 
-done
+# vim
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf $PREFIX/dotfiles/.vimrc $HOME/.vimrc
+vim +":PlugInstall"

9d8fd39407daa2401bff6419b46bf2726ea1375d
shirosuke0046 <shirosuke.0046@gmail.com>
2022-03-23 00:07:34 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PREFIX=$HOME/.config
 
 mkdir -p $PREFIX || exit
 
-git clone https://github.com/shirosuke0046/dotfiles.git
+git clone https://github.com/shirosuke0046/dotfiles.git $PREFIX/dotfiles
 
 for file in .vimrc; do
 	ln -sf $PREFIX/dotfiles/$file $HOME/$file 
